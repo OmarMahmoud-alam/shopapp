d2634d304526129169b8eff102918d1a6c626f96
omar208 <55194832+omar208@users.noreply.github.com>
2021-08-07 18:39:27 +0200

    notsolve

---
+++ lib/userdashboard/model/categro.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Categroy extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Categroy'),
+    );
+  }
+}
